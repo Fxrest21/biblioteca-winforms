3c26b99f2c724e156f5b955db37794c0a7720a3b
Fxrest21 <dafertajo5@gmail.com>
2026-03-08 23:29:05 -0600

    Creación de proyecto winforms

--- BibliotecaWinForms/Form1.Designer.cs
+++ BibliotecaWinForms/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(949, 608);
+            this.ClientSize = new System.Drawing.Size(1035, 825);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
