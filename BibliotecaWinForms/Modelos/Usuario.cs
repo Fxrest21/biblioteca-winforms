502fd5f96f33a33a59fec7a6b06e4bb388249271
Fxrest21 <dafertajo5@gmail.com>
2026-03-09 00:14:26 -0600

    crear clase usuario

---
+++ BibliotecaWinForms/Modelos/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaWinForms.Modelos
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Carnet { get; set; }
+
+        public Usuario(int id, string nombre, string carnet)
+        {
+            Id = id;
+            Nombre = nombre;
+            Carnet = carnet;
+        }
+    }
+}
