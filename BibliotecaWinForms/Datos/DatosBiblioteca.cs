395aa6d329ba5ebe3b4557caf529aa68b3e7ffec
Fxrest21 <dafertajo5@gmail.com>
2026-03-09 00:27:12 -0600

    arreglo del sistema para almacenamiento d datos

---
+++ BibliotecaWinForms/Datos/DatosBiblioteca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaWinForms.Modelos;
+
+namespace BibliotecaWinForms.Datos
+{
+    public static class DatosBiblioteca
+    {
+        public static Libro[] libros = new Libro[100];
+        public static Usuario[] usuarios = new Usuario[100];
+        public static Prestamo[] prestamos = new Prestamo[100];
+
+        public static int contadorLibros = 0;
+        public static int contadorUsuarios = 0;
+        public static int contadorPrestamos = 0;
+    }
+}
