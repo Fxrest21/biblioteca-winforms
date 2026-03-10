5048150c47ee3387624cea20d469533fd171e0f7
Fxrest21 <dafertajo5@gmail.com>
2026-03-09 23:52:36 -0600

    Leves correciones a clase

--- BibliotecaWinForms/Modelos/Libros.cs
+++ BibliotecaWinForms/Modelos/Libros.cs
@@ -12,7 +12,7 @@ namespace BibliotecaWinForms.Modelos
         public string Titulo { get; set; }
         public string Autor { get; set; }
         public int Anio { get; set; }
-        public bool Disponible { get; set; }
+        public int VecesPrestado { get; set; }
 
         public Libro(int id, string titulo, string autor, int anio)
         {
@@ -20,7 +20,7 @@ namespace BibliotecaWinForms.Modelos
             Titulo = titulo;
             Autor = autor;
             Anio = anio;
-            Disponible = true;
+            VecesPrestado = 0;
         }
     }
 }
