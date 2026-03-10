b9f469047bbfc86a2b0aee3fdb4cfc066e06dab7
Fxrest21 <dafertajo5@gmail.com>
2026-03-09 23:54:37 -0600

    correciones en clase usuario

--- BibliotecaWinForms/Modelos/Usuario.cs
+++ BibliotecaWinForms/Modelos/Usuario.cs
@@ -8,13 +8,14 @@ namespace BibliotecaWinForms.Modelos
 {
     public class Usuario
     {
-        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Carnet { get; set; }
 
-        public Usuario(int id, string nombre, string carnet)
+        public int Prestamos { get; set; }
+
+        public Usuario(string nombre, string carnet)
         {
-            Id = id;
+            Prestamos = 0;
             Nombre = nombre;
             Carnet = carnet;
         }
